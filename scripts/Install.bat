8beb051b5621ecf282d13d8d69ddde51ce3cf9a6
Uwe Kögel <15942327+yusei36@users.noreply.github.com>
2026-04-24 16:26:35 +0200

    stop old process so update works properly

--- scripts/Install.bat
+++ scripts/Install.bat
@@ -10,5 +10,6 @@ for %%f in ("%~dp0*.cer")  do set "CER=%%f"
 for %%f in ("%~dp0*.msix") do set "MSIX=%%f"
 
 certutil -addstore "TrustedPeople" "%CER%"
+PowerShell -ExecutionPolicy Bypass -Command "Stop-Process -Name KeePassPasskeyProvider -Force -ErrorAction SilentlyContinue"
 PowerShell -ExecutionPolicy Bypass -Command "Add-AppxPackage '%MSIX%'"
 start "" KeePassPasskeyProvider.exe
