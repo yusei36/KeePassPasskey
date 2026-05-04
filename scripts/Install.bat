5438ed0e82c2629f32c75675f39fecf1f8d2edf2
Uwe Kögel <15942327+yusei36@users.noreply.github.com>
2026-05-04 23:31:00 +0200

    make sure Add-AppxPackage is called with RunAsCurrentUser & improved console output of install script

--- scripts/Install.bat
+++ scripts/Install.bat
@@ -9,7 +9,17 @@ if %errorlevel% neq 0 (
 for %%f in ("%~dp0*.cer")  do set "CER=%%f"
 for %%f in ("%~dp0*.msix") do set "MSIX=%%f"
 
+echo Installing certificate...
 certutil -addstore "TrustedPeople" "%CER%"
+
+echo Stopping existing KeePassPasskeyProvider process...
 PowerShell -ExecutionPolicy Bypass -Command "Stop-Process -Name KeePassPasskeyProvider -Force -ErrorAction SilentlyContinue"
-PowerShell -ExecutionPolicy Bypass -Command "Add-AppxPackage '%MSIX%'"
-start "" KeePassPasskeyProvider.exe
+
+echo Installing MSIX package...
+PowerShell -ExecutionPolicy Bypass -Command "Start-Process PowerShell -Verb RunAsCurrentUser -Wait -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -Command \"Add-AppxPackage ''''%MSIX%''''\"'"
+
+echo Starting KeePassPasskeyProvider...
+PowerShell -ExecutionPolicy Bypass -Command "Start-Process KeePassPasskeyProvider.exe -Verb RunAsCurrentUser"
+
+echo Done.
+timeout /t 5
